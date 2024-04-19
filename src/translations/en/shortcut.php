87e21c38a9be2ef53cc4b89e6bc3c6da9f222aea
Josh Crawford <joshua@sgroup.com.au>
2024-04-19 22:17:43 +1000

    Update English translations.

--- src/translations/en/shortcut.php
+++ src/translations/en/shortcut.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'Could not find the url for element {id}' => 'Could not find the url for element {id}',
+  'Could not find the url for element {id}' => 'Could not find the url for element {id}',
 ];
